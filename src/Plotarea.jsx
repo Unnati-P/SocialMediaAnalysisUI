f6e5e5a941a6c732cf689c61c09e610e0237ec4b
Unnati-P <unnati.parekh502@gmail.com>
2017-12-12 01:19:03 +0530

    Updates the PlotArea for remaining plots

--- src/Plotarea.jsx
+++ src/Plotarea.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import _ from 'lodash';
 import PropTypes from 'prop-types';
-import {Pie} from 'react-chartjs-2';
+import {Pie, HorizontalBar, Bar, Doughnut} from 'react-chartjs-2';
 import axios from 'axios';
 
 const getRetweetChartData = data => {
@@ -26,6 +26,106 @@ const getRetweetChartData = data => {
     }
 };
 
+const getPollutionTweetsLocationData = data => {
+    const labels = _.keys(data);
+
+    return {
+        labels,
+        datasets: [{
+            data: _.map(data, function (value) {
+                return value;
+            }),
+            label: 'Number of tweets',
+            backgroundColor: 'rgba(255,99,132,0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            borderWidth: 1,
+            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+            hoverBorderColor: 'rgba(255,99,132,1)',
+            height: 1500,
+        }]
+    };
+};
+
+const getOckhiTweetsLocationData = data => {
+    const labels = _.keys(data);
+
+    return {
+        labels,
+        datasets: [{
+            data: _.map(data, function (value) {
+                return value;
+            }),
+            label: 'Number of tweets',
+            backgroundColor: 'rgba(255,99,132,0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            borderWidth: 1,
+            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+            hoverBorderColor: 'rgba(255,99,132,1)',
+            height: 1500,
+        }]
+    };
+};
+
+const getPollutionTweetsFavoritesData = data => {
+    const labels = _.keys(data);
+
+    return {
+        labels,
+        datasets: [{
+            data: _.map(data, function (value) {
+                return value;
+            }),
+            label: 'Favorite Count',
+            backgroundColor: 'rgba(255,99,132,0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            borderWidth: 1,
+            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+            hoverBorderColor: 'rgba(255,99,132,1)',
+        }]
+    };
+};
+
+const getOckhiTweetsFavoritesData = data => {
+    const labels = _.keys(data);
+
+    return {
+        labels,
+        datasets: [{
+            data: _.map(data, function (value) {
+                return value;
+            }),
+            label: 'Favorite Count',
+            backgroundColor: 'rgba(255,99,132,0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            borderWidth: 1,
+            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+            hoverBorderColor: 'rgba(255,99,132,1)',
+        }]
+    };
+};
+
+const getPollutionTweetsMediaData = data => {
+    const labels = _.keys(data);
+
+    return {
+        labels,
+        datasets: [{
+            data: _.map(data, function (value) {
+                return value;
+            }),
+            label: 'Tweets with image and image + text',
+            backgroundColor: [
+                '#FF6384',
+                '#36A2EB',
+            ],
+            hoverBackgroundColor: [
+                '#FF6384',
+                '#36A2EB',
+            ]
+        }]
+    };
+};
+
 
 class Plotarea extends React.Component {
     constructor(props) {
@@ -33,7 +133,13 @@ class Plotarea extends React.Component {
 
         this.state = {
             loading: false,
-            pieChartData: {}
+            pieChartData: {},
+            pollutionTweetsLocationData: {},
+            pollutionTweetsFavoritesData: {},
+            pollutionTweetsMediaData: {},
+            ockhiTweetsLocationData: {},
+            ockhiTweetsFavoritesData: {},
+            ockhiTweetsMediaData: {},
         };
     }
 
@@ -42,7 +148,7 @@ class Plotarea extends React.Component {
 
         if (that.props.activeGraph === 0) {
             that.setState({loading: true});
-            axios.get('http://127.0.0.1:3045/getpollutionretweets')
+            axios.get('http://127.0.0.1:5000/getpollutionretweets')
                 .then(function (res) {
                     that.setState({loading: false});
                     that.setState({pieChartData: res.data})
@@ -51,7 +157,7 @@ class Plotarea extends React.Component {
 
         if (that.props.activeGraph === 1) {
             that.setState({loading: true});
-            axios.get('http://127.0.0.1:3045/getockhiretweets')
+            axios.get('http://127.0.0.1:5000/getockhiretweets')
                 .then(function (res) {
                     that.setState({loading: false});
                     that.setState({pieChartData: res.data})
@@ -65,7 +171,7 @@ class Plotarea extends React.Component {
         if (that.props.activeGraph !== nextProps.activeGraph) {
             if (nextProps.activeGraph === 0) {
                 that.setState({loading: true});
-                axios.get('http://127.0.0.1:3045/getpollutionretweets')
+                axios.get('http://127.0.0.1:5000/getpollutionretweets')
                     .then(function (res) {
                         that.setState({loading: false});
                         that.setState({pieChartData: res.data})
@@ -73,10 +179,58 @@ class Plotarea extends React.Component {
             }
             if (nextProps.activeGraph === 1) {
                 that.setState({loading: true});
-                axios.get('http://127.0.0.1:3045/getockhiretweets')
+                axios.get('http://127.0.0.1:5000/getockhiretweets')
                     .then(function (res) {
                         that.setState({loading: false});
                         that.setState({pieChartData: res.data})
+                    });
+            }
+            if (nextProps.activeGraph === 2) {
+                that.setState({loading: true});
+                axios.get('http://127.0.0.1:5000/getpollutiontweetslocations')
+                    .then(function (res) {
+                        that.setState({loading: false});
+                        that.setState({pollutionTweetsLocationData: res.data.results})
+                    });
+            }
+            if (nextProps.activeGraph === 3) {
+                that.setState({loading: true});
+                axios.get('http://127.0.0.1:5000/getpollutiontweetsfavorites')
+                    .then(function (res) {
+                        that.setState({loading: false});
+                        that.setState({pollutionTweetsFavoritesData: res.data.results})
+                    });
+            }
+            if (nextProps.activeGraph === 4) {
+                that.setState({loading: true});
+                axios.get('http://127.0.0.1:5000/getpollutiontweetsmedia')
+                    .then(function (res) {
+                        that.setState({loading: false});
+                        that.setState({pollutionTweetsMediaData: res.data.results})
+                    });
+            }
+            if (nextProps.activeGraph === 5) {
+                that.setState({loading: true});
+                axios.get('http://127.0.0.1:5000/getockhitweetslocations')
+                    .then(function (res) {
+                        that.setState({loading: false});
+                        that.setState({ockhiTweetsLocationData: res.data.results})
+                    });
+            }
+            if (nextProps.activeGraph === 6) {
+                that.setState({loading: true});
+                axios.get('http://127.0.0.1:5000/getockhitweetsfavorites')
+                    .then(function (res) {
+                        that.setState({loading: false});
+                        that.setState({ockhiTweetsFavoritesData: res.data.results})
+                    });
+            }
+            if (nextProps.activeGraph === 7) {
+                that.setState({loading: true});
+                axios.get('http://127.0.0.1:5000/getockhitweetsmedia')
+                    .then(function (res) {
+                        that.setState({loading: false});
+                        that.setState({ockhiTweetsMediaData: res.data.results})
                     });
             }
         }
@@ -89,7 +243,7 @@ class Plotarea extends React.Component {
             return (
                 <div className="loaderWrapper">
                     <div className="loader">
-                        {'We\'re loading the data, please wait...'}
+                        {'We\'re loading the data. This may take upto 1 minute, please wait...'}
                     </div>
                 </div>
             );
@@ -99,7 +253,7 @@ class Plotarea extends React.Component {
                     <div
                         className="heading"
                     >
-                        {`${this.state.pieChartData.retweeted} tweets were retweeted and ${this.state.pieChartData.not_retweeted} tweets were not retweeted in case of pollution and smog`}
+                        {`${this.state.pieChartData.retweeted} are retweeted and ${this.state.pieChartData.not_retweeted} tweets are original tweets`}
                     </div>
                     <div>
                         <Pie
@@ -114,7 +268,7 @@ class Plotarea extends React.Component {
                     <div
                         className="heading"
                     >
-                        {`${this.state.pieChartData.retweeted} tweets were retweeted and ${this.state.pieChartData.not_retweeted} tweets were not retweeted in case of ockhi storm`}
+                        {`${this.state.pieChartData.retweeted} are retweeted and ${this.state.pieChartData.not_retweeted} tweets are original tweets`}
                     </div>
                     <div>
                         <Pie
@@ -123,8 +277,97 @@ class Plotarea extends React.Component {
                     </div>
                 </div>
             );
-        }
-        else {
+        } else if (that.props.activeGraph === 2) {
+            return (
+                <div className="plotArea">
+                    <div
+                        className="heading"
+                    >
+                        {'This graph shows the locations of users who tweeted'}
+                    </div>
+                    <div>
+                        <HorizontalBar
+                            data={getPollutionTweetsLocationData(this.state.pollutionTweetsLocationData)}
+                        />
+                    </div>
+                </div>
+            );
+        } else if (that.props.activeGraph === 3) {
+            return (
+                <div className="plotArea">
+                    <div
+                        className="heading"
+                    >
+                        {'This graph shows favorite count of original tweets'}
+                    </div>
+                    <div>
+                        <Bar
+                            data={getPollutionTweetsFavoritesData(this.state.pollutionTweetsFavoritesData)}
+                        />
+                    </div>
+                </div>
+            );
+        } else if (that.props.activeGraph === 4) {
+            return (
+                <div className="plotArea">
+                    <div
+                        className="heading"
+                    >
+                        {'Distribution of type of tweets'}
+                    </div>
+                    <div>
+                        <Doughnut
+                            data={getPollutionTweetsMediaData(this.state.pollutionTweetsMediaData)}
+                        />
+                    </div>
+                </div>
+            );
+        } else if (that.props.activeGraph === 5) {
+            return (
+                <div className="plotArea">
+                    <div
+                        className="heading"
+                    >
+                        {'This graph shows the locations of users who tweeted'}
+                    </div>
+                    <div>
+                        <HorizontalBar
+                            data={getOckhiTweetsLocationData(this.state.ockhiTweetsLocationData)}
+                        />
+                    </div>
+                </div>
+            );
+        } else if (that.props.activeGraph === 6) {
+            return (
+                <div className="plotArea">
+                    <div
+                        className="heading"
+                    >
+                        {'This graph shows favorite count of original tweets'}
+                    </div>
+                    <div>
+                        <Bar
+                            data={getOckhiTweetsFavoritesData(this.state.ockhiTweetsFavoritesData)}
+                        />
+                    </div>
+                </div>
+            );
+        } else if (that.props.activeGraph === 7) {
+            return (
+                <div className="plotArea">
+                    <div
+                        className="heading"
+                    >
+                        {'Distribution of type of tweets'}
+                    </div>
+                    <div>
+                        <Doughnut
+                            data={getPollutionTweetsMediaData(this.state.ockhiTweetsMediaData)}
+                        />
+                    </div>
+                </div>
+            );
+        }else {
             return <div>{'Could not load graph'}</div>
         }
     }
