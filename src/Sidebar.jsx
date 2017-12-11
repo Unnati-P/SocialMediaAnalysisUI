296abef7804549bd1cd3cfb9168a389b65d65d4f
Unnati-P <unnati.parekh502@gmail.com>
2017-12-12 01:19:34 +0530

    Updates the sidebar for different plots

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -4,6 +4,22 @@ import _ from 'lodash';
 import classnames from 'classnames';
 
 class Sidebar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showPollution: true,
+            showOckhi: true,
+        };
+    }
+
+    onPollutionClick = () => {
+        this.setState({showPollution: !this.state.showPollution})
+    };
+
+    onOckhiClick = () => {
+        this.setState({showOckhi: !this.state.showOckhi})
+    };
+
     onClick(graphNumber) {
         this.props.onSideBarItemClick(graphNumber);
     }
@@ -13,35 +29,68 @@ class Sidebar extends React.Component {
 
         return (
             <div className="sidebar">
-                <div
-                    className={classnames('sidebar__item', {active: this.props.activeGraph === 0})}
-                    onClick={_.partial(that.onClick.bind(that), 0)}
-                >
-                    <div>{'Pollution Retweets Curve'}</div>
+                <div className="main-heading">
+                    <div onClick={this.onPollutionClick}>{'Pollution Data Analysis'}</div>
+                    {this.state.showPollution && (
+                        <div>
+                            <div
+                                className={classnames('sidebar__item', {active: this.props.activeGraph === 0})}
+                                onClick={_.partial(that.onClick.bind(that), 0)}
+                            >
+                                <div>{'Pollution Retweets Chart'}</div>
+                            </div>
+                            <div
+                                className={classnames('sidebar__item', {active: this.props.activeGraph === 2})}
+                                onClick={_.partial(that.onClick.bind(that), 2)}
+                            >
+                                {'Pollution tweets Users/Location Chart'}
+                            </div>
+                            <div
+                                className={classnames('sidebar__item', {active: this.props.activeGraph === 3})}
+                                onClick={_.partial(that.onClick.bind(that), 3)}
+                            >
+                                {'Pollution tweets Favorite count Chart'}
+                            </div>
+                            <div
+                                className={classnames('sidebar__item', {active: this.props.activeGraph === 4})}
+                                onClick={_.partial(that.onClick.bind(that), 4)}
+                            >
+                                {'Pollution tweets with image and texts'}
+                            </div>
+                        </div>
+                    )}
                 </div>
-                <div
-                    className={classnames('sidebar__item', {active: this.props.activeGraph === 1})}
-                    onClick={_.partial(that.onClick.bind(that), 1)}
-                >
-                    {'Ockhi Retweets Curve'}
-                </div>
-                <div
-                    className={classnames('sidebar__item', {active: this.props.activeGraph === 2})}
-                    onClick={_.partial(that.onClick.bind(that), 2)}
-                >
-                    {'Third Graph'}
-                </div>
-                <div
-                    className={classnames('sidebar__item', {active: this.props.activeGraph === 3})}
-                    onClick={_.partial(that.onClick.bind(that), 3)}
-                >
-                    {'Fourth Graph'}
-                </div>
-                <div
-                    className={classnames('sidebar__item', {active: this.props.activeGraph === 4})}
-                    onClick={_.partial(that.onClick.bind(that), 4)}
-                >
-                    {'Fifth Graph'}
+
+                <div className="main-heading">
+                    <div onClick={this.onOckhiClick}>{'Ockhi Storm Data Analysis'}</div>
+                    {this.state.showOckhi && (
+                        <div>
+                            <div
+                                className={classnames('sidebar__item', {active: this.props.activeGraph === 1})}
+                                onClick={_.partial(that.onClick.bind(that), 1)}
+                            >
+                                {'Ockhi Retweets Chart'}
+                            </div>
+                            <div
+                                className={classnames('sidebar__item', {active: this.props.activeGraph === 5})}
+                                onClick={_.partial(that.onClick.bind(that), 5)}
+                            >
+                                {'Ockhi storm tweets Users/Location Chart'}
+                            </div>
+                            <div
+                                className={classnames('sidebar__item', {active: this.props.activeGraph === 6})}
+                                onClick={_.partial(that.onClick.bind(that), 6)}
+                            >
+                                {'Ockhi tweets Favorite count Chart'}
+                            </div>
+                            <div
+                                className={classnames('sidebar__item', {active: this.props.activeGraph === 7})}
+                                onClick={_.partial(that.onClick.bind(that), 7)}
+                            >
+                                {'Pollution tweets with image and texts'}
+                            </div>
+                        </div>
+                    )}
                 </div>
             </div>
         );
